2da674f2d11a8a988681fe45ddc59c8e6d12b619
emanuelefavero <emafaux89@gmail.com>
2024-10-17 19:43:41 +0200

    remove children in TextParallaxContent, remove ExampleContent

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { ReactNode, useRef } from 'react'
+import { useRef } from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
-import { FiArrowUpRight } from 'react-icons/fi'
 
 export default function Component() {
   return (
@@ -11,23 +10,17 @@ export default function Component() {
         imgUrl='https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         subheading='Collaborate'
         heading='Built for all of us.'
-      >
-        <ExampleContent />
-      </TextParallaxContent>
+      />
       <TextParallaxContent
         imgUrl='https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         subheading='Quality'
         heading='Never compromise.'
-      >
-        <ExampleContent />
-      </TextParallaxContent>
+      />
       <TextParallaxContent
         imgUrl='https://images.unsplash.com/photo-1504610926078-a1611febcad3?q=80&w=2416&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         subheading='Modern'
         heading='Dress for the best.'
-      >
-        <ExampleContent />
-      </TextParallaxContent>
+      />
     </div>
   )
 }
@@ -38,12 +31,10 @@ const TextParallaxContent = ({
   imgUrl,
   subheading,
   heading,
-  children,
 }: {
   imgUrl: string
   subheading: string
   heading: string
-  children: ReactNode
 }) => {
   return (
     <div
@@ -56,7 +47,6 @@ const TextParallaxContent = ({
         <StickyImage imgUrl={imgUrl} />
         <OverlayCopy heading={heading} subheading={subheading} />
       </div>
-      {children}
     </div>
   )
 }
@@ -126,26 +116,3 @@ const OverlayCopy = ({
     </motion.div>
   )
 }
-
-const ExampleContent = () => (
-  <div className='mx-auto grid max-w-5xl grid-cols-1 gap-8 px-4 pb-24 pt-12 md:grid-cols-12'>
-    <h2 className='col-span-1 text-neutral-600 text-3xl font-bold md:col-span-4'>
-      Additional content explaining the above card here
-    </h2>
-    <div className='col-span-1 md:col-span-8'>
-      <p className='mb-4 text-xl text-neutral-600 md:text-2xl'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
-        blanditiis soluta eius quam modi aliquam quaerat odit deleniti minima
-        maiores voluptate est ut saepe accusantium maxime doloremque nulla
-        consectetur possimus.
-      </p>
-      <p className='mb-8 text-xl text-neutral-600 md:text-2xl'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-        reiciendis blanditiis aliquam aut fugit sint.
-      </p>
-      <button className='w-full rounded bg-neutral-900 px-9 py-4 text-xl text-white transition-colors hover:bg-neutral-700 md:w-fit'>
-        Learn more <FiArrowUpRight className='inline' />
-      </button>
-    </div>
-  </div>
-)
