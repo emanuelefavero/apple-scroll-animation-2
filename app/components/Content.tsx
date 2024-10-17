3ad9965f55ec4b59832dbfd09a9999cdb87410fd
emanuelefavero <emafaux89@gmail.com>
2024-10-17 19:48:16 +0200

    remove white background

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -5,7 +5,7 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 
 export default function Component() {
   return (
-    <div className='bg-white'>
+    <>
       <TextParallaxContent
         imgUrl='https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         subheading='Collaborate'
@@ -21,7 +21,7 @@ export default function Component() {
         subheading='Modern'
         heading='Dress for the best.'
       />
-    </div>
+    </>
   )
 }
 
