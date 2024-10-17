30b23bd66fb0e5049a03b474c6e5f7a6578fc5a2
emanuelefavero <emafaux89@gmail.com>
2024-10-17 19:57:34 +0200

    change heading and subheading text

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -8,18 +8,18 @@ export default function Component() {
     <>
       <TextParallaxContent
         imgUrl='https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        subheading='Collaborate'
-        heading='Built for all of us.'
+        subheading='Subheading 1'
+        heading='Heading 1'
       />
       <TextParallaxContent
         imgUrl='https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        subheading='Quality'
-        heading='Never compromise.'
+        subheading='Subheading 2'
+        heading='Heading 2'
       />
       <TextParallaxContent
         imgUrl='https://images.unsplash.com/photo-1504610926078-a1611febcad3?q=80&w=2416&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        subheading='Modern'
-        heading='Dress for the best.'
+        subheading='Subheading 3'
+        heading='Heading 3'
       />
     </>
   )
